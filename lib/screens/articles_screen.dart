9251fd1f8d6b6ec14981e1cc2e9027b2544b0eb4
bashiryousufy <bashiryousufy@gmail.com>
2023-05-18 01:26:02 +0800

    screens

--- lib/screens/articles_screen.dart
+++ lib/screens/articles_screen.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class ArticleScreen extends StatelessWidget {
+  const ArticleScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Home");
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Articles'),
+        centerTitle: true,
+      ),
+    );
   }
 }
