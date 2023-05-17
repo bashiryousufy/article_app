7ac58ddf8f00c6d2eab6f33cff0ccdf8a5baa21b
bashiryousufy <bashiryousufy@gmail.com>
2023-05-18 02:20:12 +0800

    implement query articles

--- lib/services/article_service.dart
+++ lib/services/article_service.dart
@@ -5,26 +5,18 @@ import 'package:article_app/constants/config.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ArticleService {
+  static String NYT_URL = '${Config.API_URL}?api-key=${Config.API_KEY}';
+
   static Future<List<Article>> getMostViewedArticles() async {
     try {
+      List<Article> articles = await getCachedArticles();
+
+      if (articles.isNotEmpty) return articles;
+
       final SharedPreferences prefs = await SharedPreferences.getInstance();
 
-      // Check if articles are cached
-      if (prefs.containsKey('articles')) {
-        final String articlesString = prefs.getString('articles') ?? '';
-
-        final List<dynamic> articles = jsonDecode(articlesString);
-
-        return articles.map((article) => Article.fromJson(article)).toList();
-      }
-
-      List<Article> articles = [];
-
-      final String url =
-          '${Config.API_URL}?api-key=${Config.API_KEY}&offset=0&limit=20';
-
       http.Response response = await http
-          .get(Uri.parse(url), headers: {"Accept": "application/json"});
+          .get(Uri.parse(NYT_URL), headers: {"Accept": "application/json"});
 
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
@@ -39,7 +31,47 @@ class ArticleService {
 
       return articles;
     } catch (e) {
-      print(e);
+      return [];
+    }
+  }
+
+  static Future<List<Article>> queryArticles(String query) async {
+    try {
+      List<Article> articles = [];
+
+      http.Response response = await http.get(Uri.parse('$NYT_URL&q=$query'),
+          headers: {"Accept": "application/json"});
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+
+        final List<dynamic> articleList = data['response']['docs'];
+
+        articles =
+            articleList.map((article) => Article.fromJson(article)).toList();
+      }
+
+      return articles;
+    } catch (e) {
+      return [];
+    }
+  }
+
+  static Future<List<Article>> getCachedArticles() async {
+    try {
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+      // Check if articles are cached
+      if (prefs.containsKey('articles')) {
+        final String articlesString = prefs.getString('articles') ?? '';
+
+        final List<dynamic> articles = jsonDecode(articlesString);
+
+        return articles.map((article) => Article.fromJson(article)).toList();
+      }
+
+      return [];
+    } catch (e) {
       return [];
     }
   }
