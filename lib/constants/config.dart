a0ba9fe9cb0855953a8c112fcff08f6f1e509446
bashiryousufy <bashiryousufy@gmail.com>
2023-05-26 12:36:40 +0800

    Not a good idea to push secrets but ...

---
+++ lib/constants/config.dart
@@ -0,0 +1,13 @@
+class Config {
+  static final String SEARCH_API_URL =
+      "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+  static final String MOSTVIEWED_API_URL =
+      "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json";
+
+  static const String API_KEY = "4d76KflucxHneh5e8baxKKTTUnwcIKv3";
+
+  static const String API_SECRET = "ROtqMjfHojqHhdb3";
+
+  static const String API_APPID = "45be8487-2e15-4fe5-82bd-2ad013785e48";
+}
